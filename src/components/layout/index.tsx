e2cb55f33f10cd9bdf3d2e99e291bdd9a88af0d2
Hamza-Tasadaq <hamzatasadaq01@gmail.com>
2023-08-10 12:19:26 +0500

    Fixed

--- src/components/layout/index.tsx
+++ src/components/layout/index.tsx
@@ -1,29 +1,29 @@
-import { FC, ReactNode } from "react";
-import { useSelector } from "react-redux";
+import { FC, ReactNode, useCallback, useState } from "react";
 import classNames from "classnames";
-import Header from "@/components/Header";
-import Sidebar from "@/components/Sidebar";
-import { RootState } from "@/store/types";
+import Sidebar from "./sidebar";
+import Header from "./header";
 
 interface LayoutProps {
   children: ReactNode;
 }
 
 const Layout: FC<LayoutProps> = ({ children }) => {
-  const openSidebar = useSelector(
-    (state: RootState) => state.globalReducer.openSidebar
-  );
+  const [openSidebar, setOpenSidebar] = useState(false);
 
   const mainContentClassName = classNames("flex-1 duration-300 ml-0 md:ml-56", {
     "ml-0": !openSidebar,
     "md:ml-56": openSidebar,
   });
 
+  const toggleSidebar = useCallback(() => {
+    setOpenSidebar((openSidebar) => !openSidebar);
+  }, []);
+
   return (
     <div className="relative min-h-screen flex w-full">
-      <Sidebar />
+      <Sidebar openSidebar={openSidebar} toggleSidebar={toggleSidebar} />
       <div className={mainContentClassName}>
-        <Header />
+        <Header toggleSidebar={toggleSidebar} />
         <div className="p-3 sm:p-5 md:p-8 lg:p-10 ">{children}</div>
       </div>
     </div>
