7f50f078e23e918b2fe9aa1793338274f218f87a
Hamza-Tasadaq <hamzatasadaq01@gmail.com>
2023-08-21 13:10:12 +0500

    Logout Modal Implemented

--- src/components/layout/sidebar/index.tsx
+++ src/components/layout/sidebar/index.tsx
@@ -1,5 +1,4 @@
 import { FC } from "react";
-import { useRouter } from "next/navigation";
 import Image from "next/image";
 import { AiOutlineCloseCircle as CloseCircle } from "react-icons/ai";
 import { HiOutlineUserCircle as UserCircle } from "react-icons/hi";
@@ -12,7 +11,6 @@ import MenuItem from "./MenuItem";
 import {
   HomeIcon,
   RiderIcon,
-  LogoutIcon,
   ReportIcon,
   BrandIcon,
   VariationIcon,
@@ -27,7 +25,7 @@ import {
   BarcodeIcon,
   PurchaseIcon,
 } from "@/assets/icons";
-import { signOut } from "next-auth/react";
+import Logout from "./Logout";
 
 interface SidebarProps {
   openSidebar: boolean;
@@ -35,8 +33,6 @@ interface SidebarProps {
 }
 
 const Sidebar: FC<SidebarProps> = ({ openSidebar, toggleSidebar }) => {
-  const router = useRouter();
-
   const navBarItems = [
     { label: "Home", icon: <HomeIcon />, slug: "/dashboard/home" },
     {
@@ -130,60 +126,49 @@ const Sidebar: FC<SidebarProps> = ({ openSidebar, toggleSidebar }) => {
         { label: "Business", icon: <ListIcon />, slug: "/settings/buisnesses" },
       ],
     },
-
     { label: "Reports", icon: <ReportIcon />, slug: "/dashboard/reports" },
   ];
 
-  const handleLogOut = () => {
-    signOut();
-    // router.push("/");
-  };
-
   return (
-    <div
-      className={`${
-        openSidebar ? "left-0" : "-left-80"
-      } fixed duration-300 top-0 bg-[#ffffff] md:left-0 py-5 pb-16 bottom-0 z-50 w-64 md:w-56 h-full border-r border-[#F0F0F0] flex flex-col justify-between`}
-    >
+    <>
       <div
-        onClick={toggleSidebar}
-        className="absolute md:hidden text-[#4f46e5] top-10 -right-4 cursor-pointer bg-[#ffffff] rounded-full border-r border-[#4f46e5] p-2"
-        aria-label="Close Sidebar"
+        className={`${
+          openSidebar ? "left-0" : "-left-80"
+        } fixed duration-300 top-0 bg-[#ffffff] md:left-0 py-5 pb-16 bottom-0 z-50 w-64 md:w-56 h-full border-r border-[#F0F0F0] flex flex-col justify-between`}
       >
-        <CloseCircle />
-      </div>
-      <div className="w-full px-2 flex flex-col justify-center space-y-2">
-        <div className="flex items-center justify-center">
-          <Image
-            src={"/assets/images/logo.png"}
-            alt="logo"
-            width={60}
-            height={60}
-            priority
-          />
-        </div>
-        {navBarItems.map((item, index) => {
-          const { icon, label, slug, childrens } = item;
-          return (
-            <MenuItem
-              key={index}
-              icon={icon}
-              label={label}
-              slug={slug}
-              childrens={childrens}
-            />
-          );
-        })}
-
-        <button
-          onClick={handleLogOut}
-          className="flex items-center duration-300 space-x-3 hover:text-[#4540e1] p-2 rounded-lg overflow-hidden hover:bg-[#4540e133]"
+        <div
+          onClick={toggleSidebar}
+          className="absolute md:hidden text-[#4f46e5] top-10 -right-4 cursor-pointer bg-[#ffffff] rounded-full border-r border-[#4f46e5] p-2"
+          aria-label="Close Sidebar"
         >
-          <LogoutIcon />
-          <h3>Logout</h3>
-        </button>
+          <CloseCircle />
+        </div>
+        <div className="w-full px-2 flex flex-col justify-center space-y-2">
+          <div className="flex items-center justify-center">
+            <Image
+              src={"/assets/images/logo.png"}
+              alt="logo"
+              width={60}
+              height={60}
+              priority
+            />
+          </div>
+          {navBarItems.map((item, index) => {
+            const { icon, label, slug, childrens } = item;
+            return (
+              <MenuItem
+                key={index}
+                icon={icon}
+                label={label}
+                slug={slug}
+                childrens={childrens}
+              />
+            );
+          })}
+          <Logout />
+        </div>
       </div>
-    </div>
+    </>
   );
 };
 
